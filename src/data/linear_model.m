d21dc88ac868dc9f3eab3d15473a879376284985
erramuzpe <asier.erramuzpe@gmail.com>
2017-08-04 10:48:58 +0200

    added linear model function

---
+++ src/data/linear_model.m
@@ -0,0 +1,10 @@
+function [ weights, p_values ] = linear_model(X, y)
+%LINEAR_MODEL Summary of this function goes here
+%   Detailed explanation goes here
+
+[weigths,dev,stats] = glmfit(X,y);
+
+p_values = stats.p;
+
+end
+
