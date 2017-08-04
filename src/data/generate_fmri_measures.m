567d0101aa1c6600e8649b5f6dcd3597a16750db
erramuzpe <asier.erramuzpe@gmail.com>
2017-08-04 10:08:21 +0200

    added fmri measures calculation script

---
+++ src/data/generate_fmri_measures.m
@@ -0,0 +1,23 @@
+clear all; clc;
+
+load('/home/asier/git/ppg-fmri-igabilon-17/data/raw/subjects_name.mat');
+
+fmri_path = strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/');
+
+for i=1:length(files)
+    outfilename = files{i} % prints the file we are analyzing
+    
+    load(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/raw/data_fmri/',...
+        outfilename,'/time_series.txt'));
+    time_series = time_series';
+
+    mean_fmri = mean(time_series)';
+    std_fmri = std(time_series)';
+    energy_fmri = calc_energy(time_series)';
+    pp_fmri = point_process(time_series)';
+
+    save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_mean'), 'mean_fmri')
+    save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_std'), 'std_fmri')
+    save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_energy'), 'energy_fmri')
+    save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_pp'), 'pp_fmri')
+end  
