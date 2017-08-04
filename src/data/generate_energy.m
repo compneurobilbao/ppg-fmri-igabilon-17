131a14e496926a2127e18901dd5952b975d44672
erramuzpe <asier.erramuzpe@gmail.com>
2017-08-04 10:48:42 +0200

    generated all measures in single matrixes for lineal model calculation

--- src/data/generate_energy.m
+++ src/data/generate_energy.m
@@ -14,9 +14,17 @@ for i=1:length(files)
     energy_HRV = calc_energy(HRV);
     energy_HRV_LF = calc_energy(HRV_LF);
     energy_HRV_HF = calc_energy(HRV_HF);
+    
+    energy_HRV_all(i) = energy_HRV;
+    energy_HRV_LF_all(i) = energy_HRV_LF;
+    energy_HRV_HF_all(i) = energy_HRV_HF;
 
     save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/energy/', outfilename, '_energy_HRV'), 'energy_HRV')
     save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/energy/', outfilename, '_energy_HRV_LF'), 'energy_HRV_LF')
     save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/energy/', outfilename, '_energy_HRV_HF'), 'energy_HRV_HF')
 end  
 
+
+save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/energy/energy_HRV_all'), 'energy_HRV_all')
+save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/energy/energy_HRV_LF_all'), 'energy_HRV_LF_all')
+save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/energy/energy_HRV_HF_all'), 'energy_HRV_HF_all')
--- src/data/generate_fmri_measures.m
+++ src/data/generate_fmri_measures.m
@@ -15,9 +15,20 @@ for i=1:length(files)
     std_fmri = std(time_series)';
     energy_fmri = calc_energy(time_series)';
     pp_fmri = point_process(time_series)';
+    
+    mean_fmri_all(:,i) = mean_fmri;
+    std_fmri_all(:,i) = std_fmri;
+    energy_fmri_all(:,i) = energy_fmri;
+    pp_fmri_all(:,i) = pp_fmri;
 
-    save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_mean'), 'mean_fmri')
-    save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_std'), 'std_fmri')
-    save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_energy'), 'energy_fmri')
-    save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_pp'), 'pp_fmri')
+%     save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_mean'), 'mean_fmri')
+%     save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_std'), 'std_fmri')
+%     save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_energy'), 'energy_fmri')
+%     save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/', outfilename, '_pp'), 'pp_fmri')
 end  
+
+
+save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/mean_all'), 'mean_fmri_all')
+save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/std_all'), 'std_fmri_all')
+save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/energy_all'), 'energy_fmri_all')
+save(strcat('/home/asier/git/ppg-fmri-igabilon-17/data/processed/fmri/pp_all'), 'pp_fmri_all')
