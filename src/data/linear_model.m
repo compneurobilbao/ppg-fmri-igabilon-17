0a99a7bbe7cec677ce2a6c843377b3d801b9b953
erramuzpe <asier.erramuzpe@gmail.com>
2017-08-08 16:12:12 +0200

    modified linear_model ?¿

--- src/data/linear_model.m
+++ src/data/linear_model.m
@@ -2,7 +2,9 @@ function [ weights, p_values ] = linear_model(X, y)
 %LINEAR_MODEL Summary of this function goes here
 %   Detailed explanation goes here
 
-[weigths,dev,stats] = glmfit(X,y);
+[weigths,dev,stats] = glmfit(X',y');
+
+% mdl = fitglm(X',y');
 
 p_values = stats.p;
 
